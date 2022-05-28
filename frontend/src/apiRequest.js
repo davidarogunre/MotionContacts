712380945d2f58fdd6d1c507a3ae98ed61a7170a
davidarogunre <davidarogunre13@outlook.com>
2022-05-28 11:07:50 +0100

    added backend files

--- frontend/src/apiRequest.js
+++ frontend/src/apiRequest.js
@@ -1,13 +1,5 @@
 const apiRequest = async (url='', optionsObj= null, errMsg=null) =>{
-    try{
-        const response = await fetch(url, optionsObj)
-        if(!response.ok){
-            throw Error('There is a problem with the server')
-        }
-    }catch(err){
-        errMsg = err.message
-    }finally{
-        return errMsg 
-    }
+    const response = await fetch(url, optionsObj)
+    return response
 }
 export default apiRequest
