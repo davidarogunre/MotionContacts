ed8134be682b94261ce889ca29424d555631b646
David Arogunre <davidarogunre13@outlook.com>
2022-07-29 19:12:26 +0100

    encrypted

--- backend/crud.py
+++ backend/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from passlib.hash import pbkdf2_sha256
-from . import models, schemas
+import models, schemas
 
 def get_user(db:Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
