c5005cc2209f91097ff91377ffbcfc540f0eb03c
David Arogunre <davidarogunre13@outlook.com>
2022-07-29 15:01:36 +0100

    added database from railway

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:AMarachi1972$@localhost/motioncontacts"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:LJajkqnBUkJNDvtCHLlJ@containers-us-west-35.railway.app:7246/railway"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
