a6ab4db1f6b4482e2a813d4f32de5fce96338a46
David Arogunre <davidarogunre13@outlook.com>
2022-07-30 08:28:42 +0100

    working

--- backend/main.py
+++ backend/main.py
@@ -111,4 +111,4 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="https://motioncontacts-production.up.railway.app/", port=5000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
