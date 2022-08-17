a46bbd462097050ea4704265ef358ca39ac079c0
David Arogunre <davidarogunre13@outlook.com>
2022-08-17 22:08:39 +0100

    Made it completely responsive and finish home welcome page

--- frontend/src/components/Home/Intro.tsx
+++ frontend/src/components/Home/Intro.tsx
@@ -6,18 +6,19 @@ import '@fontsource/roboto/100.css'
 
 function Intro() {
   return (
-    <Flex padding="0" width="90%" margin={{base:"40px auto 0 auto", "2xl":"200px auto 0 auto"}} height="500px" alignItems="center">
-        <Box backgroundColor="white" width="50%" height="250px" fontFamily="Roboto" fontWeight="400">
-          <Box fontSize={{base:"40px", md:"50px", xl:"60px"}} id="header" width="110%">
+    <Flex direction={{base:"column-reverse", md:"row"}} padding="0" maxWidth="1600px" width="90%" margin={{base:"10px auto 0 auto", md:"25px auto 0 auto", "2xl":"200px auto 0 auto"}} height="500px" alignItems="center">
+        <Box backgroundColor="white" width={{base:"90%", md:"50%"}} height="250px" fontFamily="Roboto" fontWeight="400">
+          <Box fontSize={{base:"40px", md:"50px", xl:"70px"}} id="header" width="110%" textAlign={{base:"center", md:"initial"}}>
             MotionContacts
           </Box>
-          <Box id="desc" width={{md:"100%", xl:"70%"}} fontWeight="100" marginTop="10px">
+          <Box id="desc" width={{md:"100%", xl:"70%"}} fontWeight="100" marginTop="10px" textAlign={{base:"center", md:"initial"}}>
               Looking for a better contacts apps, MotionContacts is here.
               MotionContacts is the best contacts app to suit your needs packed
               with features you never knew existed and very fast.
           </Box>
-          <Button colorScheme="blue" borderRadius="5px" variant="solid" marginTop="25px">Register</Button>
-
+          <Box display={{base:"flex", md:"block"}} justifyContent={{base:"center"}}>
+          <Button colorScheme="blue" borderRadius="5px" variant="solid" marginTop={{base:"24px", md:"25px"}}>Register</Button>
+          </Box>
         </Box>
         <Spacer/>
         <Image src={illustration} backgroundColor="orange" width={{base:"45%", "2xl":"900px"}} height={{md:"40vw", '2xl':"900px"}}/>
--- frontend/src/components/Home/Navbar.tsx
+++ frontend/src/components/Home/Navbar.tsx
@@ -5,11 +5,11 @@ import '@fontsource/roboto/400.css'
 function Navbar() {
   const listStyleRemove = {listStyle:"none"}
   return (
-    <Box width="90%" backgroundColor="white" height="60px" margin="16px auto 0 auto" display="flex" justifyContent="space-between" fontFamily="Roboto">
+    <Box width="90%" maxWidth="1600px" backgroundColor="white" height="60px" margin="16px auto 0 auto" display="flex" justifyContent="space-between" fontFamily="Roboto">
         <Box width="10%" height="inherit" display="flex" alignItems="center" fontSize="25px">
             Logo
         </Box>
-        <Box width={{md:"350px", xl:"450px"}} display="flex" justifyContent="space-between" height="inherit" alignItems="center" fontSize="19px">
+        <Box width={{md:"350px", xl:"450px"}} display={{base:"none", md:"flex"}} justifyContent="space-between" height="inherit" alignItems="center" fontSize="19px">
             <li style={listStyleRemove}>
                 Features
             </li>
@@ -20,7 +20,7 @@ function Navbar() {
                 Pricing
             </li>
         </Box>
-        <Box width={{md:"150px", xl:"175px"}} display="flex" justifyContent="space-between" height="inherit" alignItems="center">
+        <Box width={{base:"150px", xl:"175px"}} display="flex" justifyContent="space-between" height="inherit" alignItems="center">
             <Box>Login</Box>
             <Button colorScheme="blue" borderRadius="5px" variant="solid">Register</Button>
         </Box>
